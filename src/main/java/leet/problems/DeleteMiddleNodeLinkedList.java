66a43ad7f536ce99dc47ab3e17f0399e9f9228ee
Audrey Tan <audrey.wanmun.tan@gmail.com>
2025-07-16 10:34:17 -0500

    Delete Middle Node Linked List Leetcode completed

--- src/main/java/leet/problems/DeleteMiddleNodeLinkedList.java
+++ src/main/java/leet/problems/DeleteMiddleNodeLinkedList.java
@@ -9,10 +9,20 @@ public class DeleteMiddleNodeLinkedList {
         ListNode(int val, ListNode next) { this.val = val; this.next = next; }
   }
     public ListNode deleteMiddle(ListNode head){
-//validation for single node to return null
-        if(head.next == null) {
-            return null;
+        //create ListNode to point to the node before the head as placeholder for middle node
+        ListNode temp = new ListNode(0, head);
+        //initialize a pointer for a counter that reaches the end at the head of the list
+        //and a pointer that is behind end pointer to mark the middle node at the node before head
+        ListNode middleNode = temp, endNode = head;
+        //while loop that if endNode and next endNode value is not null
+        while(endNode != null && endNode.next != null){
+            //then move endNode up 2 indices and middleNode up 1
+            middleNode = middleNode.next;
+            endNode = endNode.next.next;
         }
-        //initialize a count o
+        //delete middle node by setting it equal to the .next.next value
+        middleNode.next = middleNode.next.next;
+        //return the head
+        return temp.next;
     }
 }
